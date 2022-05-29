98cc07bde1511ff7470c543ecb9388e61a0c5c41
Marcel-Mudrak <marcel-mudrak@wp.pl>
2022-05-29 19:20:31 +0200

    Looks a lot better + templates

--- random_wiki.py
+++ random_wiki.py
@@ -1,24 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 
+url_pl = 'https://pl.wikipedia.org/wiki/Specjalna:Losowa_strona'
+url_en = 'https://en.wikipedia.org/wiki/Special:Random'
+
 def choose_lang():
-    url_pl = 'https://pl.wikipedia.org/wiki/Specjalna:Losowa_strona'
-    url_en = 'https://en.wikipedia.org/wiki/Special:Random'
-    
+    pl = False
     lang = input('Please choose a language (EN/PL): ')
     
     if lang.lower() == 'en':
-        return url_en
+        return pl
     elif lang.lower() == 'pl':
-        return url_pl
+        pl = True
+        return pl
     return None
 
 def get_website():
-    url = choose_lang()
+    lang = choose_lang()
+    if lang:
+        url = url_pl
+        lang = 'pl'
+    else:
+        url = url_en
+        lang = 'en'
         
     response = requests.get(url)
     page_dom = BeautifulSoup(response.text, 'html.parser')
-    title = page = page_dom.find('h1').getText()
+    title = page_dom.find('h1').getText()
         
     print(title, '\n----------')
     user_conf = input('Would you like to see this website? (Y/N)')
@@ -26,12 +34,23 @@ def get_website():
     if user_conf.lower() == 'y':
         try:
             page = page_dom.find_all('p')
-            return title, page
+            site = []
+            
+            for line in page:
+                site.append(line.getText())
+                  
+            return title, lang, site
         except:
             return None
 
 website = get_website()
 
 if website != None:
-    with open(f'websites/{website[0]}.html', 'w', encoding='UTF-8') as file:
-        file.write(str(website))
+    with open(f'websites/{website[0]}.html', 'a', encoding='UTF-8') as file:
+        with open(f'templates/html_{website[1]}.txt', 'r', encoding='UTF-8') as template:
+            for line in template:
+                file.write(str(line))
+                
+            file.write('<h1>' + website[0] + '</h1>\n')
+            for line in website[2]:
+                file.write('<p>' + line.strip('[').strip(']').strip('\n') + '</p>')
